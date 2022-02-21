999d715ffc8fc19072b2a550a496e7e2732c32da
NazerkeAdilgali <96619528+NazerkeAdilgali@users.noreply.github.com>
2022-02-21 08:25:18 +0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+
+const notifElem = document.querySelector('.notification')
+
+const notif = {
+    open: (color = 'pink') => notifElem.classList.add('go', color),
+    close: () => notifElem.classList.remove('go')
+}
+
+setTimeout(() => {
+    notif.open()
+    setTimeout(() => {
+        notif.close()
+    }, 15000);
+}, 5000);
